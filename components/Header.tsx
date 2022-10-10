9b9afeecece6ae874408dd5361625ca1897f4071
kaszubski <mateusz.kaszubski.dev@gmail.com>
2022-10-10 17:03:16 +0200

    tailwind and styling - inProgress

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,12 @@
+import Link from 'next/link';
+
 export function Header() {
   return (
-    <header className="mx-auto w-full max-w-md">
-      <nav className="bg-gray-700 px-4 py-2 text-white">Nav</nav>
+    <header className="max-w-5xl mx-auto w-full">
+      <nav className="bg-gray-700 px-4 py-2 text-white">
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+      </nav>
     </header>
   );
 }
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,6 +1,13 @@
+import { Footer } from '../components/Footer';
+import { Header } from '../components/Header';
+
 function AboutPage() {
   return (
-    <div>About page</div>
+    <>
+      <Header />
+      <h4>About page</h4>
+      <Footer />
+    </>
   );
 }
 
